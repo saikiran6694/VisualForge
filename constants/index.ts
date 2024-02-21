f9af72d62ace8ec34b1033a2f1862010f0ce407a
saikiran6694 <saikiran1605bembalge@gmail.com>
2024-02-21 14:00:47 +0530

    payment gateway

--- constants/index.ts
+++ constants/index.ts
@@ -71,7 +71,7 @@ export const plans = [
     _id: 2,
     name: "Pro Package",
     icon: "/assets/icons/free-plan.svg",
-    price: 40,
+    price: 499,
     credits: 120,
     inclusions: [
       {
@@ -96,7 +96,7 @@ export const plans = [
     _id: 3,
     name: "Premium Package",
     icon: "/assets/icons/free-plan.svg",
-    price: 199,
+    price: 1499,
     credits: 2000,
     inclusions: [
       {
